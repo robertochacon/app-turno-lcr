b3e5861e96a21e8fcdd06f65a3c66cd0d387a36d
rchaconalcantara <hola.hack09@gmail.com>
2023-03-20 13:47:01 -0400

    New changes - 3

---
+++ src/app/components/turns/search-turn.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchTurnPipe } from './search-turn.pipe';
+
+describe('SearchTurnPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchTurnPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
